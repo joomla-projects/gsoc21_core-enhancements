4158bf64c9632d6cfbd3e161852b007b48d55e55
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-07-29 20:52:56 +0530

    improve the logic for conditionally adding transition field

--- administrator/components/com_modules/src/Model/ModuleModel.php
+++ administrator/components/com_modules/src/Model/ModuleModel.php
@@ -920,7 +920,21 @@ class ModuleModel extends AdminModel implements WorkflowModelInterface
 		Form::addFormPath(JPATH_ADMINISTRATOR . '/components/com_modules/models/forms');
 		$form->loadFile('advanced', false);
 
-		$this->workflowPreprocessForm($form, $data);
+		if (ComponentHelper::getParams('com_modules')->get('workflow_enabled'))
+		{
+			// The workflowPreprocessForm function enables the workflow plugins and adds the transition field
+			// to the form. We do not need to add the transition field to the form in the case of new
+			// items so we seperately call the importWorkflowPlugins function.
+			if (!empty(Factory::getApplication()->input->get('id')))
+			{
+				$this->workflowPreprocessForm($form, $data);
+			}
+			else
+			{
+				// Import the workflow plugin group to allow form manipulation.
+				$this->importWorkflowPlugins();
+			}
+		}
 
 		// Trigger the default form events.
 		parent::preprocessForm($form, $data, $group);
